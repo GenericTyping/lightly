80e77c886039f04eea35f274d7696ebb0c36fa6a
Jeroen Meijer <jeroenfkmeijer@gmail.com>
2021-12-21 22:22:20 +0100

    feat: complete node firebase server

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -1,3 +1,55 @@
-export const runApp = (args: string[]) => {
-  console.log('Hello, world!');
+import fetch from 'cross-fetch';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from '../firebaseConfig';
+import { getFirestore, doc, onSnapshot } from 'firebase/firestore';
+
+type RgbValue = [number, number, number];
+
+type RgbDoc = Record<string, RgbValue>;
+
+const localServerConfig = {
+  host: '192.168.68.135',
+  port: 1234,
+};
+
+const lightsCollectionName = 'lights';
+const lightsDocName = 'current';
+
+export const runApp = async () => {
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getFirestore(firebaseApp);
+
+  const unsub = onSnapshot(
+    doc(db, `${lightsCollectionName}/${lightsDocName}`),
+    (doc) => {
+      const data = doc.data() as RgbDoc;
+      console.log('Data updated!', data);
+      setLight(data);
+    }
+  );
+
+  process.on('SIGINT', () => {
+    console.log('Quitting...');
+    unsub();
+    process.exit(0);
+  });
+};
+
+const setLight = async (rgb: RgbDoc) => {
+  const res = await fetch(
+    `http://${localServerConfig.host}:${localServerConfig.port}/set`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(rgb),
+    }
+  );
+  if (!res.ok) {
+    console.error('Failed to set light', {
+      status: res.status,
+      statusText: res.statusText,
+    });
+  }
 };
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,4 @@
-import * as process from 'process';
+// import * as process from 'process';
 import { runApp } from './app';
 
-runApp(process.argv);
+runApp();
